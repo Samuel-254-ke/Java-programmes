753db95c39cd45dc973cabc4857fc10bc361e082
Sirmih <102431524+Sirmih@users.noreply.github.com>
2022-03-26 21:36:55 +0300

    Add files via upload
    this code creates a dialog box

---
+++ Dialogbox.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dialogbox;
+
+/**
+ *
+ * @author MYKAL
+ */
+public class Dialogbox {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
